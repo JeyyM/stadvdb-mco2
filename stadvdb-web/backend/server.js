e23a9ce14dadbb4bcb7aa36239c6e842afb6d4a0
JeyM <jeymson8000@gmail.com>
2025-11-30 23:40:58 +0800

    Something weird is going on

--- stadvdb-web/backend/server.js
+++ stadvdb-web/backend/server.js
@@ -7,7 +7,7 @@ const RecoveryManager = require('./recoveryManager');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+/// Middleware
 app.use(cors());
 app.use(express.json());
 
